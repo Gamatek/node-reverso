8e285b57951c5a5e17c69ae1283ede258d1496c5
Gamatek <93871422+Gamatek@users.noreply.github.com>
2023-07-14 17:09:09 +0200

    Update reverso.js

--- reverso.js
+++ reverso.js
@@ -27,30 +27,39 @@ const ReversoLanguages = {
     Ukrainian: "ukr"
 }
 
+/**
+ * Translate a text with Reverso.net.
+ * @param {String} input The input string.
+ * @param {String} from The source language.
+ * @param {String} to The target language.
+ * @param {Object} options 
+ *     @param {boolean} options.sentenceSplitter Indicates whether sentence splitting is enabled. Default value is false.
+ *     @param {string} options.origin The origin of the data. Default value is "translation.web".
+ *     @param {boolean} options.contextResults Indicates whether to include contextual results. Default value is false.
+ *     @param {boolean} options.languageDetection Indicates whether language detection is enabled. Default value is false.
+ * @returns {Promise<String>} The translated text
+ */
 const reverso = (
     input,
     from,
     to,
-    {
-        sentenceSplitter = false,
-        origin = "translation.web",
-        contextResults = false,
-        languageDetection = false
-    }
+    options
 ) => new Promise((resolve, reject) => {
     const https = require("https");
+
+    options = Object.assign({
+        sentenceSplitter: false,
+        origin: "translation.web",
+        contextResults: false,
+        languageDetection: false
+    }, options);
 
     const playload = JSON.stringify({
         format: "text",
         from,
         to,
         input,
-        options: {
-            sentenceSplitter,
-            origin,
-            contextResults,
-            languageDetection
-        }
+        options
     });
 
     const req = https.request({
@@ -68,7 +77,7 @@ const reverso = (
         res.on("data", (chunk) => chunks.push(chunk));
         res.on("end", () => {
             const bodyParsed = JSON.parse(Buffer.concat(chunks).toString("utf-8"));
-            resolve(bodyParsed);
+            resolve(bodyParsed.translation[0], bodyParsed);
         });
     });
 
