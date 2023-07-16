eacbf6f6265ed32e11db6bd53964814d90338c13
Gamatek <93871422+Gamatek@users.noreply.github.com>
2023-07-16 22:47:22 +0200

    Update reverso.js

--- reverso.js
+++ reverso.js
@@ -1,3 +1,5 @@
+const axios = require("axios");
+
 const ReversoLanguages = {
     Arabic: "ara",
     Chinese: "chi",
@@ -25,7 +27,7 @@ const ReversoLanguages = {
     Thai: "tha",
     Turkish: "tur",
     Ukrainian: "ukr"
-}
+};
 
 /**
  * Translate a text with Reverso.net.
@@ -37,53 +39,30 @@ const ReversoLanguages = {
  *     @param {string} options.origin The origin of the data. Default value is "translation.web".
  *     @param {boolean} options.contextResults Indicates whether to include contextual results. Default value is false.
  *     @param {boolean} options.languageDetection Indicates whether language detection is enabled. Default value is false.
- * @returns {Promise<Object>} The api response body.
+ * @returns {Promise<String>} Translation from your test.
  */
-const reverso = (
-    input,
-    from,
-    to,
-    options
-) => new Promise((resolve, reject) => {
-    const https = require("https");
-
-    options = Object.assign({
-        sentenceSplitter: false,
-        origin: "translation.web",
-        contextResults: false,
-        languageDetection: false
-    }, options);
-
-    const playload = JSON.stringify({
-        format: "text",
-        from,
-        to,
-        input,
-        options
-    });
-
-    const req = https.request({
-        hostname: "api.reverso.net",
-        path: "/translate/v1/translation",
+const reverso = (input, from, to) => new Promise((resolve, reject) => {
+    axios("https://api.reverso.net/translate/v1/translation", {
         method: "POST",
         headers: {
-            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-            "Content-Type": "application/json",
-            "Content-Length": playload.length
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+        },
+        data: {
+            format: "text",
+            from,
+            to,
+            input,
+            options: {
+                sentenceSplitter: true,
+                origin: "translation.web",
+                contextResults: true,
+                languageDetection: true
+            }
         }
-    }, (res) => {
-        const chunks = [];
-        res.on("error", (err) => reject(err));
-        res.on("data", (chunk) => chunks.push(chunk));
-        res.on("end", () => {
-            const bodyParsed = JSON.parse(Buffer.concat(chunks).toString("utf-8"));
-            resolve(bodyParsed);
-        });
-    });
-
-    req.on("error", (err) => reject(err));
-    req.write(playload);
-    req.end();
+    }).then((res) => {
+        const { translation } = res.data;
+        resolve(translation.join(""), res);
+    }).catch(reject);
 });
 
 module.exports = { reverso, ReversoLanguages };
